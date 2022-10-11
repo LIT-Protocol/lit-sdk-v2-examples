00567ef30457e205a2a3c44989468bab310d3c22
Adarsh-Kumar28 <adarsh@litprotocol.com>
2022-10-11 12:09:53 +0530

    feat: Add NFT component

--- encrypt_and_decrypt_on-chain_nft_metadata/src/App.js
+++ encrypt_and_decrypt_on-chain_nft_metadata/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Nft from './Nft';
 
 function App() {
+
+  const sampleNft = { name: "Sample NFT", image: "https://www.w3schools.com/images/w3schools_green.jpg", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!" };
+
   return (
     <div className="App">
-      Hello
+      <h1>Encrypt & Decrypt an On-Chain NFT Metadata using Lit SDK</h1>
+      <div className='nfts'>
+        <Nft nft={sampleNft} />
+      </div>
     </div>
   );
 }
---
+++ encrypt_and_decrypt_on-chain_nft_metadata/src/Nft.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Nft.css'
+
+export default function Nft({ nft }) {
+  return (
+    <div className='nft'>
+        <h2>{ nft.name }</h2>
+        <div className='nft__divider'></div>
+        <div className='nft__details'>
+            <img src={nft.image} />
+            { nft.description }
+        </div>
+    </div>
+  )
+}

c80b2e7939f4fed7cd53a3452ddc29187b275456
Adarsh-Kumar28 <adarsh@litprotocol.com>
2022-10-13 14:57:18 +0530

    feat: Close modal after submit & checked correctness of minting

--- encrypt_and_decrypt_on-chain_nft_metadata/src/Modal.js
+++ encrypt_and_decrypt_on-chain_nft_metadata/src/Modal.js
@@ -6,14 +6,26 @@ export default function Modal({ mintLitNft, closeModal }) {
 	const [description, setDescription] = useState("");
 	const [imageUrl, setImageUrl] = useState("");
 
+	const submitMintLitNft = async () => {
+		mintLitNft(name, imageUrl, description);
+		closeModalAndRemoveContents();
+	}
+
+	const closeModalAndRemoveContents = () => {
+		setName("");
+		setDescription("");
+		setImageUrl("");
+		closeModal();
+	}
+
   return (
     <div className='modal'>
-			<span className='modal__close' onClick={closeModal}>&times;</span>
+			<span className='modal__close' onClick={closeModalAndRemoveContents}>&times;</span>
 			<h2>Enter NFT Details</h2>
 			<input value={name} className='modal__input' type="text" required placeholder="Name" onChange={e => setName(e.target.value)} />
 			<textarea value={description} className='modal__description' type="text" required onChange={e => setDescription(e.target.value)} placeholder="Description" />
 			<input value={imageUrl} className='modal__input' type="text" required placeholder="Image URL" onChange={e => setImageUrl(e.target.value)} />
-			<button disabled={!name || !description || !imageUrl} onClick={() => mintLitNft(name, imageUrl, description)} className='modal__button'>Mint</button>
+			<button disabled={!name || !description || !imageUrl} onClick={submitMintLitNft} className='modal__button'>Mint</button>
     </div>
   )
 }

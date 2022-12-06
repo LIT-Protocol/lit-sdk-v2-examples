5b94d509f0ceacb1869351da2e70fe8460d8b54c
Adarsh-Kumar28 <adarsh@litprotocol.com>
2022-12-06 22:45:37 +0530

    feat: Encrypt & Decrypt string with Custom EVM Acc

--- custom-emv-acc/src/App.js
+++ custom-emv-acc/src/App.js
@@ -1,9 +1,59 @@
+import { useState } from 'react';
+import lit from './lit';
 import './App.css';
 
 function App() {
+  const noAuthError = "The access control condition check failed! You should have at least 1 Zora Custom Drop NFT to decrypt this string.";
+
+  const [text, setText] = useState("");
+  const [encryptedText, setEncryptedText] = useState(null);
+  const [encryptedSymmetricKey, setEncryptedSymmetricKey] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+
+  const encryptText = async () => {
+    if (text.length === 0) {
+      alert("Please enter a non-empty string!");
+      return;
+    }
+
+    setDecryptedText("");
+
+    const { encryptedString, encryptedSymmetricKey } = await lit.encryptText(text);
+    setEncryptedText(encryptedString);
+    setEncryptedSymmetricKey(encryptedSymmetricKey);
+  }
+
+  const decryptText = async () => {
+    if (encryptedText === null) {
+      alert("Please encrypt your string first!");
+      return;
+    }
+
+    try {
+      const decryptedString = await lit.decryptText(encryptedText, encryptedSymmetricKey);
+      setDecryptedText(decryptedString);
+    } catch (error) {
+      alert(noAuthError);
+    }
+  }
+
   return (
     <div className="App">
-      Hello
+      <h1>Encrypt & Decrypt a string using Lit SDK</h1>
+      <div className="textAreas">
+        <textarea type="text" onChange={e => setText(e.target.value)} placeholder="Type here..." />
+        <textarea readOnly value={decryptedText} className="readOnly" />
+      </div>
+      <div>
+        <button onClick={encryptText}>Encrypt</button>
+        <button onClick={decryptText}>Decrypt</button>
+      </div>
+      {(encryptedText !== null && decryptedText.length === 0) && (
+        <h3>String Encrypted: {encryptedText.size} bytes. Thanks for using Lit!</h3>
+      )}
+      {decryptedText.length > 0 && (
+        <h3>String Decrypted: See the right text area</h3>
+      )}
     </div>
   );
 }
---
+++ custom-emv-acc/src/lit.js
@@ -0,0 +1,88 @@
+import LitJsSdk from "@lit-protocol/sdk-browser";
+
+const client = new LitJsSdk.LitNodeClient();
+const chain = "ethereum";
+
+// Checks if the user has at least 1 Zora Custom Drop NFT
+const evmContractConditions = [{
+    contractAddress: "0x115b90187d38dC0A9A9d6BdC8EC9b1f492964894",
+    chain: "ethereum",
+    functionName: "balanceOf",
+    // functionParams: ["0x7028956e9dBF459775Cc2E071BE4331B711E56Fa"],
+    functionParams: [":userAddress"],
+    functionAbi: {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    returnValueTest: {
+      key: "",
+      comparator: ">=",
+      value: "0",
+    },
+}];
+
+class Lit {
+  litNodeClient;
+
+  async connect() {
+    await client.connect();
+    this.litNodeClient = client;
+  }
+
+  async encryptText(text) {
+    if (!this.litNodeClient) {
+        await this.connect();
+    }
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain });
+    const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(text);
+
+    const encryptedSymmetricKey = await this.litNodeClient.saveEncryptionKey({
+        evmContractConditions,
+        symmetricKey,
+        authSig,
+        chain,
+    });
+
+    return {
+        encryptedString,
+        encryptedSymmetricKey: LitJsSdk.uint8arrayToString(encryptedSymmetricKey, "base16")
+    };
+  }
+
+  async decryptText(encryptedString, encryptedSymmetricKey) {
+    if (!this.litNodeClient) {
+        await this.connect();
+    }
+
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({ chain });
+    const symmetricKey = await this.litNodeClient.getEncryptionKey({
+        evmContractConditions,
+        toDecrypt: encryptedSymmetricKey,
+        chain,
+        authSig
+    });
+
+    const decryptedString = await LitJsSdk.decryptString(
+        encryptedString,
+        symmetricKey
+    );
+    return decryptedString;
+  }
+}
+
+export default new Lit();

a653ce86857fa6ad413e11a38b5c2446d338c3b4
Adarsh-Kumar28 <adarsh@litprotocol.com>
2023-01-16 14:54:30 +0530

    feat: UI layout

--- smart-contract-authsig/src/App.js
+++ smart-contract-authsig/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Smart Contract Authsig
+      <h1>Smart Contract Authsig</h1>
+      <div className="container">
+        <div className="inputContainer">
+          <h2>String to encrypt</h2>
+          <input type="text" />
+          <h2>Unhashed _hash parameter for isValidSugnature</h2>
+          <input type="text" />
+          <h2>Address of the Smart Contract</h2>
+          <input type="text" />
+          <h2>Chain</h2>
+          <input type="text" />
+        </div>
+        <div className="authSig">
+          <h2>Authsig Generated</h2>
+        </div>
+      </div>
+      <div className="buttonsContainer">
+        <button>Encrypt</button>
+        <button>Decrypt</button>
+      </div>
     </div>
   );
 }
